c59a5e430c2be7059936a9f3fd69ffdf97295232
Andrew Kouré <50218177+ankoure@users.noreply.github.com>
2026-02-16 09:29:20 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Use vendored protoc
     let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
-    unsafe {
-        std::env::set_var("PROTOC", protoc_path);
-    }
-    prost_build::compile_protos(
+
+    let mut config = prost_build::Config::new();
+    config.protoc_path(protoc_path);
+
+    config.compile_protos(
         &["proto/gtfs-realtime.proto"], // input proto
         &["proto/"],                    // proto include path
     )?;
